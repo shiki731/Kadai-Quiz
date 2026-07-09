c3c6671c694ded83794149ffe4d68732f176e522
shiki <ktc25A31A0022@edu.kyoto-tech.ac.jp>
2026-07-10 03:12:28 +0900

    aaa

---
+++ Assets/Script/ButtonSystem.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ButtonSystem : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickRightButton()
+    {
+        Debug.Log("aaa");
+    }
+
+    public void OnClickLeftButton()
+    {
+        Debug.Log("bbb");
+    }
+}
