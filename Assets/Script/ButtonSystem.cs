2f2bf59e238c9dcc270f4e84e1c9494af638efdd
shiki <ktc25A31A0022@edu.kyoto-tech.ac.jp>
2026-07-14 03:02:22 +0900

    aaa

--- Assets/Script/ButtonSystem.cs
+++ Assets/Script/ButtonSystem.cs
@@ -1,26 +1,112 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class ButtonSystem : MonoBehaviour
 {
+    private bool IsPressed = false;
+    private bool bol;
+    private int QuestPattern = 0;
+    private bool ok;
+
+    public TextMeshProUGUI mondai;
+    public TextMeshProUGUI buttonL;
+    public TextMeshProUGUI buttonR;
+
+    public enum Mode
+    {
+        WaitInput,
+        Start,
+        Question,
+        WaitAnswer,
+        Answer
+    }
+    Mode Main = Mode.WaitInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        IsPressed = false;
+        bol = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (Main)
+        {
+            case Mode.WaitInput:
+                buttonR.text = "Žn‚ß‚é";
+                buttonL.text = "@";
+                if (IsPressed)
+                {
+                    Main = Mode.Start;
+                    IsPressed = false;
+                    StartCoroutine(StartCount());
+                }
+                break;
+            case Mode.Start:
+                buttonL.text = "Z";
+                buttonR.text = "~";
+                if (bol)
+                {
+                    QuestPattern = QuestPattern + 1;
+                    Main = Mode.Question;
+                    Debug.Log(QuestPattern);
+                }
+                break;
+            case Mode.Question:
+                if(QuestPattern == 1)
+                {
+                    mondai.text = "–â‘è‚P";
+                }
+                else if (QuestPattern == 2)
+                {
+                    mondai.text = "–â‘è‚Q";
+                }
+                else if (QuestPattern == 3)
+                {
+                    mondai.text = "–â‘è‚R";
+                }
+                else if (QuestPattern == 4)
+                {
+                    mondai.text = "–â‘è‚S";
+                }
+                Main = Mode.WaitAnswer;
+                break;
+            case Mode.WaitAnswer:
+                if (ok == true)
+                {
+                    ok = false;
+                    Main = Mode.Start;
+                }
+                break;
+        }
+    }
+
+    IEnumerator StartCount()
+    {
+        for(int i = 3;i > 0; i--)
+        {
+            mondai.text = i.ToString();
+            yield return new WaitForSeconds(0.5f);
+        }
+        bol = true;
     }
 
     public void OnClickRightButton()
     {
-        Debug.Log("aaa");
+        IsPressed = true;
+        if(QuestPattern == 1 || QuestPattern == 3)
+        {
+            ok = true;
+        }
     }
 
     public void OnClickLeftButton()
     {
-        Debug.Log("bbb");
+        if (QuestPattern == 2 || QuestPattern == 4)
+        {
+            ok = true;
+        }
     }
 }
